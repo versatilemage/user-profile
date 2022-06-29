9281957dc17f82b7f77387726908a3a6203d5dee
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-06-29 14:45:57 +0530

    Code Cleanup

--- js/editprofile.js
+++ js/editprofile.js
@@ -7,40 +7,50 @@ let retrival = JSON.parse(localStorage.getItem("localData"))
 function editingProfile() {
     const savedNames = retrival.profile.filter((item) => item.userid === profileId)
     const MaptheSaved = savedNames.map((item) => {
-        return `<div class=""user-row>
-    <label>First Name</label>
+        return `<div class="form-row">
+    <label>FirstName</label>
     <input type="text" id="changeofFname" value="${item.user.name.first}">
-    <br><br>
-    <label>Last Name</label>
+    </div>
+    <div class="form-row">
+    <label>LastName</label>
     <input type="text" id="changeofLname" value="${item.user.name.last}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Title</label>
     <input type="text" id="changeofTitle" value="${item.user.name.title}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Team</label>
     <input type="text" id="changeofteam" value="${item.team}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Job</label>
     <input type="text" id="changeofadd" value="${item["job-title"]}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Contact</label>
     <input type="number" id="changeofphn" value="${item.user.contact}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Email</label>
     <input type="email" id="changeofMail" value="${item.user.email}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Upload</label>
-    <input type="text" id="Myfile" value="${item.user.picture.thumbnail}">
+    <input type="text" id="Myfile" value="${item.user.picture.large}">
     <input type="hidden" id="Myfile-Medium" value="${item.user.picture.thumbnail}">
     <input type="hidden" id="Myfile-Large" value="${item.user.picture.thumbnail}">
-    <div><span>note:</span>use only url's for image</div>
-    <br><br>
+    <div class="note"><span>note:</span>use only url's for image</div>
+    </div>
+    <div class="form-row hidden-row">
     <label>Last-login</label>
     <input type="hidden" id="todayDate" value="${item["last-login"].datetime}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Join-Date</label>
     <input type="text" id="joiningDate"  value="${item.user["join-date"]}">
-    <br><br>
+    </div>
+    <div class="form-row">
     <label>Online Status</label>
     <input type="text" id="0nOrof" value="${item.loggedIn}">
     </div>
@@ -50,11 +60,11 @@ function editingProfile() {
 }
 
 function saveChanges() {
-    let today = new Date();
+    var today = new Date();
     const result = today.getDate();
     const fullYear = (today.getFullYear())
-    let forzeromonth = today.toLocaleDateString("en-Gb", { month: "2-digit" })
-    let forTime = today.toLocaleTimeString()
+    var forzeromonth = today.toLocaleDateString("en-Gb", { month: "2-digit" })
+    var forTime = today.toLocaleTimeString()
 
     let item = {}
     item.user = {}
@@ -79,6 +89,7 @@ function saveChanges() {
     retrival.profile.forEach((obj, index) => {
         if (obj.userid == item.userid) {
             retrival.profile[index] = item;
+            console.log(item, "item")
             setTimeout(function () {
                 localStorage.setItem('localData', JSON.stringify(retrival));
                 alert("Profile details have updated successfully")
--- js/profile.js
+++ js/profile.js
@@ -3,26 +3,28 @@ const urlParams = new URLSearchParams(queryString);
 const profileId = urlParams.get('id')
 
 function profileLoading() {
-let getLocalData = JSON.parse(localStorage.getItem("localData"));
+    let getLocalData = JSON.parse(localStorage.getItem("localData"));
 
-const savedFilter = getLocalData.profile.filter ((item) => item.userid === profileId)
-const MaptheFilter = savedFilter.map((item) => {
-    if (item.loggedIn === true){
-        item.loggedIn = "online"
-    }else {
-        item.loggedIn = "offline"
-    }
-    return `
-    <div class="user-thumb"><img src="${item.user.picture.thumbnail}"></div>
-    <div class="user-name"><strong>Name : </strong>${item.user.name.first} ${item.user.name.last}</div>
-    <div class="forteam"> <strong>Team : </strong> ${item.team}</div>
-    <div class="Id-No"><strong>User-Id : </strong> ${item.userid}</div>
-    <div class="forjobdetail"> <strong>Job : </strong> ${item["job-title"]}</div>
-    <div class="contactingNo"><strong>Contact Number : </strong> ${item.user.contact}</div>
-    <div class="isloggedin"> <strong>Online Status : </strong> ${item.loggedIn}</div>
+    const savedFilter = getLocalData.profile.filter((item) => item.userid === profileId)
+    const MaptheFilter = savedFilter.map((item) => {
+        if (item.loggedIn === true) {
+            item.loggedIn = `online`
+        } else {
+            item.loggedIn = `offline`
+        }
+        return `
+    <div class="user-thumb"><img src="${item.user.picture.large}"></div>
+    <div class="for-order">
+    <div class="user-name"><strong>Name</strong></div><div>${item.user.name.title}. ${item.user.name.first} ${item.user.name.last}</div>
+    <div class="forteam"> <strong>Team</strong></div><div>${item.team}</div>
+    <div class="Id-No"><strong>User-Id</strong></div><div>${item.userid}</div>
+    <div class="forjobdetail"> <strong>Job</strong></div><div>${item["job-title"]}</div>
+    <div class="contactingNo"><strong>Contact Number</strong></div><div>${item.user.contact}</div>
+    <div class="isloggedin" id="log-in-color"> <strong>Online Status</strong></div><div>${item.loggedIn}</div>
+    </div>
     `
-})
-document.getElementById("profile-details").innerHTML = MaptheFilter.join("")
+    })
+    document.getElementById("profile-details").innerHTML = MaptheFilter.join("")
 }
 function editProfile() {
     return location.href = `../html/editprofile.html?id=${profileId}`
